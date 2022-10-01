5823975228c2756652d9a14e090c7881c0b71096
ZhykharievAnatolii <nekijt87@gmail.com>
2022-10-01 11:54:27 +0300

    upd ver 1.0 homework
    hope its works

--- src/Book.js
+++ src/Book.js
@@ -1,5 +1,9 @@
-import { Author } from './Author.js';
-import { User } from './User.js';
+import {
+    Author
+} from './Author.js';
+import {
+    User
+} from './User.js';
 
 /**
  * @param {string} title
@@ -13,4 +17,43 @@ import { User } from './User.js';
  * @property {User[]} likedUsers
  * @property {User} publicationBy
  */
-export function Book(title, year, publicationBy, authors) {}
+export function Book(title, year, publicationBy, authors) {
+    this.title = title;
+    this.authors = authors;
+    this.year = year;
+    this.likedUsers = [];
+    this.publicationBy = publicationBy;
+
+    this.publicationBy.myBooks.push(this);
+
+    this.authors.forEach((author) => {
+        author.books.push(this);
+    });
+
+    Object.defineProperties(this, {
+        'suggestedBooks': {
+            get() {
+                return this.authors.reduce((array, {
+                    books
+                }) => {
+                    books.forEach(({
+                                       title
+                                   }) => this.title !== title && !array.includes(title) ? array.push(title) : array);
+                    return array;
+                }, []).join(', ');
+            }
+        },
+        'suggestedPublicators': {
+            get() {
+                return this.authors.reduce((array, {
+                    books
+                }) => {
+                    books.forEach(({
+                                       publicationBy
+                                   }) => this.publicationBy !== publicationBy && !array.includes(publicationBy.name) ? array.push(publicationBy.name) : array);
+                    return array;
+                }, []).join(', ');
+            }
+        }
+    });
+}
--- src/User.js
+++ src/User.js
@@ -1,4 +1,6 @@
-import { Book } from './Book.js';
+import {
+    Book
+} from './Book.js';
 
 /**
  * @param {string} name
@@ -10,4 +12,53 @@ import { Book } from './Book.js';
  * @property {User[]} friends
  * @property {Book[]} likes
  */
-export function User(name, date) {}
+export function User(name, date) {
+
+    this.name = name;
+    this.date = date;
+    this.myBooks = [];
+    this.friends = [];
+    this.likes = [];
+
+
+    function addRemFunc(obj, props, el) {
+        obj[props].includes(el) ? obj[props] = obj[props].filter((item) => item !== el) : obj[props].push(el);
+    }
+
+    this.addToFriends = function (user) {
+        addRemFunc(this, 'friends', user);
+        addRemFunc(user, 'friends', this);
+    };
+    this.likeBook = function (book) {
+        addRemFunc(this, 'likes', book);
+        addRemFunc(book, 'likedUsers', this);
+    };
+
+    this.removeFriend = this.addToFriends;
+    this.unlikeBook = this.likeBook;
+
+    Object.defineProperties(this, {
+        'friendsNames': {
+            get() {
+                return this.friends.map(({
+                                             name
+                                         }) => name).join(', ');
+            }
+
+        },
+        'likedBooks': {
+            get() {
+                return this.likes.map(({
+                                           title
+                                       }) => title).join(', ');
+            }
+        },
+        'publishedBooks': {
+            get() {
+                return this.myBooks.map(({
+                                             title
+                                         }) => title).join(', ');
+            }
+        }
+    });
+}
